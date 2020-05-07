c787ebbdbe41dfe13a66a4b3a0c8f54986ffd9fc
Babu Neupane <neupanebabu828@gmail.com>
2020-05-08 01:37:41 +1000

    first commit

---
+++ src/main/java/com/example/babu/demo/AwsCodePipelineApplication.java
@@ -0,0 +1,12 @@
+package com.example.babu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCodePipelineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AwsCodePipelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/babu/demo/AwsCodePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.babu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCodePipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
